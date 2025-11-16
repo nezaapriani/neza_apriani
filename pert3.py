787b4e100051622a04cb2d0f9e357bd1f09d53a6
nezaapriani <incesnezaapriani@gmail.com>
2025-11-17 06:16:57 +0800

    Add files via upload

---
+++ pert3.py
@@ -0,0 +1,21 @@
+# kita akan belajar tipe data (jenis2 data)
+
+#bilangan bulat (integer "int")
+x = 10
+print("nilai dari x :" , x)
+print(type(x))
+
+#bilangan pecahan (fload "fload")
+y = 3.5
+print("nilai dari y :", y)
+print(type(y))
+
+#tipe data string(tulisan "str")
+nama = "neza"
+print("nilai dari nama :" , nama)
+print(type(nama))
+
+#tipe data benar salah (true / false "bool")
+kebenaran = False
+print("nilai dari kebenaran :" , kebenaran)
+print(type(kebenaran))
---
+++ pert4dantugas.py
@@ -0,0 +1,22 @@
+ #mengambil imputan dari user
+ 
+nama = input("Masukkan Nama Anda :")
+print("Selamat Datang", nama)
+print(type(nama))
+
+umur = int(input("Maukkan Umur Anda  :"))
+print("Umur Anda Sekarang", umur)
+print(type (umur))
+
+kejujuran = bool(input("Apakah, kamu selingkuh :"))
+print("kejujuran mu", umur)
+print(type(kejujuran))
+
+#TUGAS
+
+nama = input("Masukkan Nama Anda:")
+nim = int(input("Maukkan Nim Anda :"))
+print("Nama Anda Adalah" , nama)
+print("NIM Anda Adalah ", nim)
+print(type (nama))
+print(type(nim))
