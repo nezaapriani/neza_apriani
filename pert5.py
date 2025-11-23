dc1f8fd89ca293bfa47bc29c72e6ba7d5ceefd1c
nezaapriani <incesnezaapriani@gmail.com>
2025-11-23 20:52:37 +0800

    Add files via upload

---
+++ pert5.py
@@ -0,0 +1,69 @@
+#operasi aritmatika
+#x / + -
+
+#operasi penjumlahan +
+a = 12
+b = 10
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+#pengurangan -
+a = 12
+b = 10
+hasil = a - b
+print(a, "-",  b, "=", hasil)
+
+#perkalian *
+a = 12
+b = 10
+hasil = a *  b
+print(a, "*", b, "=", hasil)
+
+#pembagian /
+a = 12
+b = 10
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+#perpangkatan menggunakan ** (eksponesial)
+a = 12
+b = 10
+hasil = a ** b
+print(a, "pangkat", b, "=", hasil)
+
+#sisa pembagian (modulus) %
+a = 12
+b = 10
+hasil = a % b
+print(a, "mod", b, "=", hasil)
+
+#floor division
+a = 10
+b = 3
+hasil = a // b
+print(a, "floor division", b, "=", hasil)
+
+#operasi prioritas
+   # 1()
+   # perpangkatan atau akar
+   #perkalian atau pembagian
+   #penjumlahan pengurangan
+   
+ #TUGAS
+   #buatlah operasi penjumlahan yg  menggabungkan semua operasi diatas
+   
+   # Operasi Prioritas Lengkap
+   
+(8 + 2**2) * 5 - 40 / (16**0.5)
+hasil = (8 + 2**2) * 5 - 40 / (16**0.5)
+print("Hasil operasi =", hasil)
+
+
+
+
+
+
+
+
+
+
